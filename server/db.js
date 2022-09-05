6288c7e4a2ff348d934bc8b7c6b8bf2e2166af68
Michael Nervais <mnervais@gmail.com>
2022-09-05 21:20:17 +0100

    chore: Version bump

--- server/db.js
+++ server/db.js
@@ -2,11 +2,11 @@ const Pool = require("pg").Pool;
 require("dotenv").config();
 
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
-const proConfig = process.env.DATABASE_URL; //heroku addons
+const prodConfig = process.env.DATABASE_URL; //heroku addons
 
 const pool = new Pool({
   connectionString:
-    process.env.NODE_ENV === "production" ? proConfig : devConfig,
+    process.env.NODE_ENV === "production" ? prodConfig : devConfig,
   ssl: {
     rejectUnauthorized: false,
   },
